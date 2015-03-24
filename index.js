52ec69c7e9f7045cf1c70abe364147e3bd10d60a
veselov-pv <7262830@gmail.com>
2015-03-24 17:50:48 +0100

    db name fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM data_table', function(err, result) {
+    client.query('SELECT * FROM ad_data_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
@@ -14,8 +14,8 @@ app.get('/db', function (request, response) {
        { response.send(result.rows); }
     });
   });
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
