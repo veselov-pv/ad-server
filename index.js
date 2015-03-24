c966f471d61200854af2fde508f7d8237c33e14a
veselov-pv <7262830@gmail.com>
2015-03-24 17:41:00 +0100

    heroku server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var server = requ
+var express = require('express')
+var app = express();
+var pg = require('pg');
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM data_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
