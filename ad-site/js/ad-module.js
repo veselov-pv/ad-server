cfdab4bbc03ffaac0ada41417b2aec7384062bcf
veselov-pv <7262830@gmail.com>
2015-03-29 23:06:25 +0300

    tests

--- ad-site/js/ad-module.js
+++ ad-site/js/ad-module.js
@@ -1,182 +1,186 @@
-var adModule = (function () {
+var adModule = new function () {
 	var content, closeBtn, spinner, imageWr, image, imageMirror,
 		downloadBtn, likeBtn, dislikeBtn, stopBtn, shareBtn, baseData,
-		onElementsInitFinish;
+		onElementsInitFinish, t;
 	var PORTRAIT = 'portrait';
-	var LANDSCAPE = 'landscape';
+	//var LANDSCAPE = 'landscape';
 	var BASE_URL = 'https://spherical-cow.herokuapp.com/data';
 	var isElementsInited = false;
 
-	function addClass(o, c) {
-		var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
-		if (re.test(o.className)) return;
-		o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "");
-	}
-
-	function removeClass(o, c) {
-		var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
-		o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "");
-	}
-
-	function isJsonString(str) {
-		try {
-			JSON.parse(str);
-		} catch (e) {
-			return false;
-		}
-		return true;
-	}
-
-	function checkImageDefiningSize() {
-		if (!image.naturalHeight || !image.naturalWidth) return;
-		if (imageWr.offsetHeight / imageWr.offsetWidth >= image.naturalHeight / image.naturalWidth) {
-			removeClass(imageWr, 'definingHeight');
-			addClass(imageWr, 'definingWidth');
-		} else {
-			removeClass(imageWr, 'definingWidth');
-			addClass(imageWr, 'definingHeight');
-		}
-		removeClass(image,'not-visible');
-	}
-
-	function deferredRun(callback, delay, times) {
-		times -= 1;
-		setTimeout(function () {
-			callback();
-			if (times) deferredRun(callback, delay, times);
-		}, delay);
-	}
-
-	function correctImageMirrorPosition() {
-		imageMirror.style.top = image.offsetTop + image.offsetHeight + 'px';
-		removeClass(imageMirror, 'not-visible');
-	}
-
-	function getSummaryOffset(element) {
-		var top = 0, left = 0;
-		do {
-			top += element.offsetTop || 0;
-			left += element.offsetLeft || 0;
-			element = element.offsetParent;
-		} while (element);
-
-		return {
-			top: top,
-			left: left
-		};
-	}
-
-	function correctDownloadBtnWidthAndPosition() {
-		downloadBtn.style.width = image.clientWidth + 'px';
-		var imageSummaryOffsetPos = getSummaryOffset(image);
-		downloadBtn.style.left = imageSummaryOffsetPos.left + 'px';
-		downloadBtn.style.top = imageSummaryOffsetPos.top + image.offsetHeight + 15 + 'px';
-		removeClass(downloadBtn, 'not-visible');
-	}
-
-	function recheckElements () {
-		addClass(image,'not-visible');
-		checkImageDefiningSize();
-		addClass(imageMirror, 'not-visible');
-		deferredRun(correctImageMirrorPosition, 30, 3);
-		addClass(downloadBtn, 'not-visible');
-		deferredRun(correctDownloadBtnWidthAndPosition, 30, 3);
-	}
-
-	function onImageLoad() {
-		sendGetRequest(baseData.ads[0].inbox_open);
-		addClass(spinner, 'hidden');
-		removeClass(imageWr, 'hidden');
-		recheckElements();
-	}
-
-	function getNewImage() {
-		image.src = baseData.ads[0].image_url;
-		image.onload = onImageLoad;
-		imageMirror.src = baseData.ads[0].image_url;
-	}
-
-	function initElements() {
-		content = document.querySelector('#ad-content');
-		var $get = content.querySelector.bind(content);
-		closeBtn = $get('.btn.close');
-		spinner = $get('.spinner');
-		imageWr = $get('.image-wrapper');
-		image = $get('.image');
-		imageMirror = $get('.image-mirror');
-		downloadBtn = $get('.btn.download');
-		likeBtn = $get('.btn.like');
-		dislikeBtn = $get('.btn.dislike');
-		stopBtn = $get('.btn.stop');
-		shareBtn = $get('.btn.share');
-		isElementsInited = true;
-		if (typeof onElementsInitFinish == 'function') onElementsInitFinish();
-	}
-
-	function onScreenOrientationChange() {
-		recheckElements();
-	}
-
-	function initOrientationListener() {
-		var mql = window.matchMedia('(orientation: ' + PORTRAIT + ')');
-		mql.addListener(onScreenOrientationChange);
-	}
-
-	function removeAllElements() {
-		if (content) content.parentNode.removeChild(content);
-	}
-
-	function redirectToAdSite() {
-		window.location.href = baseData.ads[0].click_url;
-	}
-
-	function initListeners() {
-		initOrientationListener();
-		closeBtn.onclick = removeAllElements;
-		image.onclick = redirectToAdSite;
-		downloadBtn.onclick = redirectToAdSite;
-		likeBtn.onclick = function () {
-			sendGetRequest(baseData.ads[0].ad_like);
-		};
-		dislikeBtn.onclick = function () {
-			sendGetRequest(baseData.ads[0].ad_dislike);
-		};
-		stopBtn.onclick = function () {
-			sendGetRequest(baseData.ads[0].ad_hide);
-		};
-		shareBtn.onclick = function () {
-			sendGetRequest(baseData.ads[0].ad_share);
-		};
-	}
-
-	function sendGetRequest(url, callback) {
-		var xhr = new XMLHttpRequest();
-
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState !== 4 || xhr.status !== 200) return;
-			if (typeof callback == 'function') callback(xhr);
-		};
-		xhr.open("GET", url, true);
-		xhr.send();
-	}
-
-	function sendInitialRequest() {
-		sendGetRequest(BASE_URL, function (xhr) {
-			if (!isJsonString(xhr.responseText)) return;
-			baseData = JSON.parse(xhr.responseText);
-			if (isElementsInited === true) {
-				getNewImage();
-			} else {
-				onElementsInitFinish = getNewImage;
-			}
-		});
-	}
-
 	return {
+		addClass: function (o, c) {
+			var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+			if (re.test(o.className)) return;
+			o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "");
+		},
+
+		removeClass: function (o, c) {
+			var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+			o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "");
+		},
+
+		isJsonString: function (str) {
+			try {
+				JSON.parse(str);
+			} catch (e) {
+				return false;
+			}
+			return true;
+		},
+
+		deferredRun: function (callback, delay, times) {
+			times -= 1;
+			var self = this;
+			setTimeout(function () {
+				callback();
+				if (times) self.deferredRun(callback, delay, times);
+			}, delay);
+		},
+
+		getSummaryOffset: function (element) {
+			var top = 0, left = 0;
+			do {
+				top += element.offsetTop || 0;
+				left += element.offsetLeft || 0;
+				element = element.offsetParent;
+			} while (element);
+
+			return {
+				top: top,
+				left: left
+			};
+		},
+
+		checkImageDefiningSize: function () {
+			if (!image.naturalHeight || !image.naturalWidth) return;
+			if (imageWr.offsetHeight / imageWr.offsetWidth >= image.naturalHeight / image.naturalWidth) {
+				t.removeClass(imageWr, 'definingHeight');
+				t.addClass(imageWr, 'definingWidth');
+			} else {
+				t.removeClass(imageWr, 'definingWidth');
+				t.addClass(imageWr, 'definingHeight');
+			}
+			t.removeClass(image, 'not-visible');
+		},
+
+		correctImageMirrorPosition: function () {
+			imageMirror.style.top = image.offsetTop + image.offsetHeight + 'px';
+			t.removeClass(imageMirror, 'not-visible');
+		},
+
+		correctDownloadBtnWidthAndPosition: function () {
+			downloadBtn.style.width = image.clientWidth + 'px';
+			var imageSummaryOffsetPos = t.getSummaryOffset(image);
+			downloadBtn.style.left = imageSummaryOffsetPos.left + 'px';
+			downloadBtn.style.top = imageSummaryOffsetPos.top + image.offsetHeight + 15 + 'px';
+			t.removeClass(downloadBtn, 'not-visible');
+		},
+
+		recheckElements: function () {
+			t.addClass(image, 'not-visible');
+			t.checkImageDefiningSize();
+			t.addClass(imageMirror, 'not-visible');
+			t.deferredRun(t.correctImageMirrorPosition, 30, 3);
+			t.addClass(downloadBtn, 'not-visible');
+			t.deferredRun(t.correctDownloadBtnWidthAndPosition, 30, 3);
+		},
+
+		onImageLoad: function () {
+			t.sendGetRequest(baseData.ads[0].inbox_open);
+			t.addClass(spinner, 'hidden');
+			t.removeClass(imageWr, 'hidden');
+			t.recheckElements();
+		},
+
+		getNewImage: function () {
+			image.src = baseData.ads[0].image_url;
+			image.onload = t.onImageLoad;
+			imageMirror.src = baseData.ads[0].image_url;
+		},
+
+		initElements: function () {
+			content = document.querySelector('#ad-content');
+			var $get = content.querySelector.bind(content);
+			closeBtn = $get('.btn.close');
+			spinner = $get('.spinner');
+			imageWr = $get('.image-wrapper');
+			image = $get('.image');
+			imageMirror = $get('.image-mirror');
+			downloadBtn = $get('.btn.download');
+			likeBtn = $get('.btn.like');
+			dislikeBtn = $get('.btn.dislike');
+			stopBtn = $get('.btn.stop');
+			shareBtn = $get('.btn.share');
+			isElementsInited = true;
+			if (typeof onElementsInitFinish == 'function') onElementsInitFinish();
+		},
+
+		onScreenOrientationChange: function () {
+			t.recheckElements();
+		},
+
+		initOrientationListener: function () {
+			var mql = window.matchMedia('(orientation: ' + PORTRAIT + ')');
+			mql.addListener(t.onScreenOrientationChange);
+		},
+
+		removeAllElements: function () {
+			if (content) content.parentNode.removeChild(content);
+		},
+
+		redirectToAdSite: function () {
+			window.location.href = baseData.ads[0].click_url;
+		},
+
+		initListeners: function () {
+			t.initOrientationListener();
+			closeBtn.onclick = t.removeAllElements;
+			image.onclick = t.redirectToAdSite;
+			downloadBtn.onclick = t.redirectToAdSite;
+			likeBtn.onclick = function () {
+				t.sendGetRequest(baseData.ads[0].ad_like);
+			};
+			dislikeBtn.onclick = function () {
+				t.sendGetRequest(baseData.ads[0].ad_dislike);
+			};
+			stopBtn.onclick = function () {
+				t.sendGetRequest(baseData.ads[0].ad_hide);
+			};
+			shareBtn.onclick = function () {
+				t.sendGetRequest(baseData.ads[0].ad_share);
+			};
+		},
+
+		sendGetRequest: function (url, callback) {
+			var xhr = new XMLHttpRequest();
+
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState !== 4 || xhr.status !== 200) return;
+				if (typeof callback == 'function') callback(xhr);
+			};
+			xhr.open("GET", url, true);
+			xhr.send();
+		},
+
+		sendInitialRequest: function () {
+			t.sendGetRequest(BASE_URL, function (xhr) {
+				if (!t.isJsonString(xhr.responseText)) return;
+				baseData = JSON.parse(xhr.responseText);
+				if (isElementsInited === true) {
+					t.getNewImage();
+				} else {
+					onElementsInitFinish = t.getNewImage;
+				}
+			});
+		},
+
 		init: function () {
-			sendInitialRequest();
-			initElements();
-			initListeners();
+			t = this;
+			t.sendInitialRequest();
+			t.initElements();
+			t.initListeners();
 		}
 	}
-})();
+};
+
+if (typeof exports != 'undefined') exports.adModule = adModule;
---
+++ ad-site/test/test.js
@@ -0,0 +1,209 @@
+var chai = require('chai'),
+	assert = chai.assert,
+	expect = chai.expect,
+	should = chai.should(),
+	jsdom = require('mocha-jsdom'),
+	app = require('../../index'),
+	adModule = require('../js/ad-module').adModule;
+
+
+describe('ad module', function () {
+
+	jsdom();
+
+	describe('addClass()', function () {
+
+		it('class name added', function () {
+			var div = document.createElement('div');
+			var newClassName = 'new-class-name';
+			adModule.addClass(div, newClassName);
+
+			expect(div.className).equal(newClassName);
+		});
+
+		it('class name added just once', function () {
+			var div = document.createElement('div');
+			var newClassName = 'new-class-name';
+			adModule.addClass(div, newClassName);
+			adModule.addClass(div, newClassName);
+
+			expect(div.className).equal(newClassName);
+		});
+
+		it('class name added without clearing of other class name', function () {
+			var div = document.createElement('div');
+			var otherClassName = 'other-class-name';
+			div.className = otherClassName;
+			var newClassName = 'new-class-name';
+			adModule.addClass(div, newClassName);
+			adModule.addClass(div, newClassName);
+			var classNameArray = div.className.split(' ');
+
+			expect(classNameArray).to.include(otherClassName);
+			expect(classNameArray).to.include(newClassName);
+		});
+
+	});
+
+	describe('removeClass()', function () {
+
+		it('class name removed', function () {
+			var div = document.createElement('div');
+			var className = 'class-name';
+			div.className = className;
+			adModule.removeClass(div, className);
+
+			expect(div.className).to.be.empty;
+		});
+
+		it('class name removed if contains', function () {
+			var div = document.createElement('div');
+			var otherClassName = 'other-class-name';
+			div.className = otherClassName;
+
+			var className = 'class-name';
+			adModule.removeClass(div, className);
+
+			expect(div.className).equal(otherClassName);
+		});
+
+		it('class name removed without clearing of other class names', function () {
+			var div = document.createElement('div');
+			var otherClassName1 = 'new-class-name-1';
+			var otherClassName2 = 'new-class-name-2';
+			var className = 'class-name';
+			div.className = otherClassName1 + ' ' + otherClassName2 + ' ' + className;
+			adModule.removeClass(div, className);
+			var classNameArray = div.className.split(' ');
+
+			expect(classNameArray).to.include.members([otherClassName1, otherClassName2]);
+			expect(classNameArray).to.not.include(className);
+		});
+
+	});
+
+	describe('isJsonString()', function () {
+		var object = {
+			a: 1,
+			string: "some string",
+			obj: {key: 'value'},
+			array: [1, 2, 'asdasd']
+		};
+
+		it('true if json', function () {
+			var json = JSON.stringify(object);
+
+			expect(adModule.isJsonString(json)).to.be.true;
+		});
+
+		it('false if not json', function () {
+			var crashedJson = JSON.stringify(object).slice(-1);
+			var emptyString = '';
+			var justString = 'Hello World!';
+
+			expect(adModule.isJsonString(crashedJson)).to.be.false;
+			expect(adModule.isJsonString(emptyString)).to.be.false;
+			expect(adModule.isJsonString(justString)).to.be.false;
+		});
+
+	});
+
+	describe('deferredRun()', function () {
+
+		it('run each time', function (done) {
+			var neededTimes = 3;
+			var delay = 30;
+			var i = 0;
+			adModule.deferredRun(function () {
+				i += 1;
+				if (i == neededTimes) done();
+			}, delay, neededTimes);
+		});
+
+	});
+
+	describe('getSummaryOffset()', function () {
+
+		it('returned result has the keys "top" and "left"', function () {
+			var div = document.createElement('div');
+			expect(adModule.getSummaryOffset(div)).to.have.key('top', 'left');
+		});
+
+	});
+
+	it('checkImageDefiningSize()', function () {
+
+		it('run without throw error', function () {
+			var fnWithAttr = function () {
+				adModule.checkImageDefiningSize();
+			};
+
+			expect(fnWithAttr).to.not.throw(Error);
+		});
+
+	});
+
+	it('correctImageMirrorPosition()', function () {
+		it('run without throw error', function () {
+			var fnWithAttr = function () {
+				adModule.correctImageMirrorPosition();
+			};
+
+			expect(fnWithAttr).to.not.throw(Error);
+		});
+	});
+
+	it('correctDownloadBtnWidthAndPosition()', function () {
+		it('run without throw error', function () {
+			var fnWithAttr = function () {
+				adModule.correctDownloadBtnWidthAndPosition();
+			};
+
+			expect(fnWithAttr).to.not.throw(Error);
+		});
+	});
+
+	it('recheckElements()', function () {
+		it('run without throw error', function () {
+			var fnWithAttr = function () {
+				adModule.recheckElements();
+			};
+
+			expect(fnWithAttr).to.not.throw(Error);
+		});
+	});
+
+	it('onImageLoad()', function () {
+	});
+
+	it('getNewImage()', function () {
+	});
+
+	it('initElements()', function () {
+	});
+
+	it('onScreenOrientationChange()', function () {
+	});
+
+	it('initOrientationListener()', function () {
+	});
+
+	it('removeAllElements()', function () {
+	});
+
+	it('redirectToAdSite()', function () {
+	});
+
+	it('initListeners()', function () {
+	});
+
+	it('sendGetRequest()', function () {
+	});
+
+	it('sendInitialRequest()', function () {
+	});
+
+	it('init()', function () {
+	});
+
+});
